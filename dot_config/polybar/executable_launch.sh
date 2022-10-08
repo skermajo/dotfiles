6392d4b9c9d961b5a12fdaf405a1404b1a81d835
skermajo <joel@skerman.au>
2022-10-09 08:32:00 +1000

    Fixed polybar launch

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,6 +1,6 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar.log
-polybar bar1 2>&1 | tee -a /tmp/polybar/log & disown
+polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 echo "Bars launched..."
